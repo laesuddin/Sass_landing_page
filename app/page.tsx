5df9bbcb5eb16d6c35b27779e475f60fd5fede04
Laes Uddin <laesuddin@gmail.com>
2025-12-17 19:30:49 +0600

    some code changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-    return <div>Hello World</div>;
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
+
+export default page
