212478a37a9140aab1e83fce1fc36e37bb605ae4
Laes Uddin <laesuddin@gmail.com>
2026-01-02 21:52:58 +0600

    CallToAction section animation completed

--- sections/CallToAction.tsx
+++ sections/CallToAction.tsx
@@ -1,15 +1,59 @@
+"use client";
+import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion";
+import { use, useEffect, useRef, useState } from "react";
+
 export default function CallToAction() {
+  const [isHovered, setIsHovered] = useState(false);
+  const animation = useRef<AnimationPlaybackControls>(null);
+  const [scope, animate] = useAnimate();
+  useEffect(() => {
+    animation.current = animate(
+      scope.current,
+      { x: "-50%" },
+      {
+        duration: 30,
+        ease: "linear",
+        repeat: Infinity,
+        repeatType: "loop",
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (animation.current) {
+      if (isHovered) {
+        animation.current.speed = 0.5;
+      } else {
+        animation.current.speed = 1;
+      }
+    }
+  }, [isHovered]);
+
   return (
     <section className="py-24">
       <div className="container mx-auto px-4 overflow-x-clip p-4 flex">
-        <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
+        <motion.div
+          ref={scope}
+          animate={{
+            x: "-50%",
+          }}
+          transition={{
+            repeat: Infinity,
+            repeatType: "loop",
+            duration: 30,
+            ease: "linear",
+          }}
+          className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium group cursor-pointer"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           {Array.from({ length: 10 }).map((_, i) => (
             <div key={i} className="flex items-center gap-16">
               <span className="text-lime-400 text-7xl">&#10038;</span>
-              <span>Try it for free</span>
+              <span className="group-hover:text-lime-400">Try it for free</span>
             </div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
